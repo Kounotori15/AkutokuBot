fc190ac171403fed056218ff0abdc13499da74ae
Kounotori15 <waoon.kouno@nifty.com>
2018-12-20 22:59:34 +0900

    あ

--- main.py
+++ main.py
@@ -1,46 +1,15 @@
 import discord
-import asyncio
-import subprocess
-import re
-import random
 
 client = discord.Client()
 
-x = ["死んで", "悪徳", "勝ち", "死ね", "うんこ", "かと。", "ない。", "クソ", "糞", "ガイジ", "ホラホラ", "ぜ。", "...", "！ｗ"]
-def compel_message_akutoku(user_message):
-    for i in x:
-        if i in user_message:
-            return True
-    return False
-
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+  print("logged in as " + client.user.name)
 
 @client.event
 async def on_message(message):
-    if re.match("こんにちは", message.content):
-        if client.user != message.author:
-            m = message.author.mentionm + "こんにちは"
+  if message.author != client.user:
+    msg = message.author.mention + " Hi."
+    await client.send_message(message.channel, msg)
 
-    if compel_message_akutoku(message.content):
-        if client.user != message.author:
-            rand = random.randint(1, 10)
-            if rand < 3:
-                m = "一興かと。"
-            elif rand < 9:
-                m = message.author.mention + "はい悪徳"
-            else:
-                m = message.author.mention + "m9 はい終命の屑～"
-            await client.send_message(message.channel, m)
-
-    if message.content.startswith("トイレに行ってもいいですか？"):
-        if client.user != message.author:
-            m = "駄目です。"
-            await client.send_message(message.channel, m)
-    
-
-client.run("DISCORD_BOT_TOKEN")
+client.run("NTI1MzA5MDg5NzEwOTk3NTI1.Dv0wJA.aLLPPg0BM7PPn--b6oJIL5i9M0U")
